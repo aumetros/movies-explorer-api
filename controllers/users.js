0c60e3cd93b9eab5998f7312bbd95222ae23f697
aumetros <aum3tros@yandex.ru>
2023-07-16 15:28:47 +0300

    Создан контроллер обновления  юзера

--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,27 @@ const getUser = (req, res) => {
 };
 
 const updateUser = (req, res) => {
-
+  const { email, name } = req.body;
+  if (!ObjectID.isValid(req.user._id)) {
+    res.status(400).send({ message: 'Неправильные данные' });
+    return;
+  }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { email, name },
+    { new: true, runValidators: true },
+  )
+    .orFail(() => new Error('Юзер не найден'))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.message === 'Юзер не найден') {
+        res.status(404).send({ message: 'Юзер не найден' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Неправильные данные' });
+      } else {
+        res.status(500).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 module.exports = {
