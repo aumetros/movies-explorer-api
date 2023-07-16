98bab72d2bee563d4fa7760864ba37c272ef61cd
aumetros <aum3tros@yandex.ru>
2023-07-16 14:22:51 +0300

    Создана схема и модель фильма

---
+++ models/movie.js
@@ -0,0 +1,76 @@
+/* eslint-disable func-names */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable object-shorthand */
+
+const mongoose = require('mongoose');
+
+const { linkReg } = require();
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return linkReg.test(v);
+      },
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return linkReg.test(v);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return linkReg.test(v);
+      },
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRu: {
+    type: String,
+    required: true,
+  },
+  nameEn: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
