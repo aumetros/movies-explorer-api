09a2767c6c5219d87c53c728ba39d8f3b7aa253f
aumetros <aum3tros@yandex.ru>
2023-07-17 18:28:12 +0300

    Установлены helmet rate-limit и cors

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
+app.use(helmet());
+
 app.use(express.json());
 app.use(cookieParser());
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+app.use(requestLogger);
+
+app.use(cors({
+  credentials: true,
+}));
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(handleErrors);
 
