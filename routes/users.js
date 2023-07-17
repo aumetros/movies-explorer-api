a009f9beb695bec667d1adeb9391153b8da9198d
aumetros <aum3tros@yandex.ru>
2023-07-17 21:10:12 +0300

    Добавлена валидация celebrate для юзера

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { getUser, updateUser } = require('../controllers/users');
+const { emailReg } = require('../utils/constants');
 
 router.get('/me', getUser);
-router.patch('/me', updateUser);
+
+router.patch('/me', celebrate({
+  body: Joi.object({
+    email: Joi.string().pattern(emailReg).required(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+}), updateUser);
+
+router.use(errors());
 
 module.exports = router;
